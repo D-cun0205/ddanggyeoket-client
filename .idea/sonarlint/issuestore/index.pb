cfe27457b2ec4094438d761f08d101bebf12224b
D-cun0205 <mmeo0205@gmail.com>
2022-06-08 23:35:20 +0900

    svelte-navigator test

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,14 @@
 
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+J
+scripts/setupTypeScript.js,f/3/f357d529da3d465c5f9f10e795934000d4f326a2
+A
+package-lock.json,f/a/fa288d1472d29beccb489a676f68739ad365fc47
+@
+rollup.config.js,2/6/26100816530e53d31ed430fdf792b464f9fb0e3e
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
 A
 public/global.css,5/d/5db2aef60e7ec22b045f6be7637d6098907080c1
--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,43 @@
 <script>
-	export let name;
+	import { Router, Route, Link } from 'svelte-navigator';
+	import Login from "./Login.svelte";
+	import PrivateRoute from "./PrivateRoute.svelte";
+	import { user } from './stores.js';
 
-	import Router from 'svelte-spa-router';
-	import routes from '~/routes';
-	import Header from '~/components/Header.svelte';
+	function handleLogout() {
+		$user = null;
+	}
 </script>
 
-<Header />
-<Router {routes} />
+<Router>
+	<header>
+		<h1>Route Test</h1>
+	</header>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+		<Link to="profile">Profile</Link>
+	</nav>
 
-	<a
-			use:
-	>Login</a>
-</main>
+	<main>
+		<Route path="login">
+			<Login />
+		</Route>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+		<Route path="/">
+			<h3>Home</h3>
+			<p>home...</p>
+		</Route>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+		<Route path="about">
+			<h3>About</h3>
+			<p>about...</p>
+		</Route>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+		<PrivateRoute path="profile" let:location>
+			<h3>Welcome {$user.username}</h3>
+			<button on:click={handleLogout}>Logout</button>
+		</PrivateRoute>
+	</main>
+</Router>
