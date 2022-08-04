7f0b0cc64307b3d69101cccfc6a7317f17e74bc3
D-cun0205 <mmeo0205@gmail.com>
2022-08-04 19:59:09 +0900

    migrate sapper to sveltekit

---
+++ .svelte-kit/runtime/env/static/private.js
@@ -0,0 +1,258 @@
+// this file is generated — do not edit it
+/** @type {import('$env/static/private').ALLUSERSPROFILE} */
+export const ALLUSERSPROFILE = "C:\\ProgramData";
+
+/** @type {import('$env/static/private').APPDATA} */
+export const APPDATA = "C:\\Users\\박상훈 Pro\\AppData\\Roaming";
+
+/** @type {import('$env/static/private').CHROME_CRASHPAD_PIPE_NAME} */
+export const CHROME_CRASHPAD_PIPE_NAME = "\\\\.\\pipe\\crashpad_33000_DHWPSMDTLKFYZHNA";
+
+/** @type {import('$env/static/private').COLOR} */
+export const COLOR = "1";
+
+/** @type {import('$env/static/private').COLORTERM} */
+export const COLORTERM = "truecolor";
+
+/** @type {import('$env/static/private').CommonProgramFiles} */
+export const CommonProgramFiles = "C:\\Program Files\\Common Files";
+
+/** @type {import('$env/static/private').CommonProgramW6432} */
+export const CommonProgramW6432 = "C:\\Program Files\\Common Files";
+
+/** @type {import('$env/static/private').COMPUTERNAME} */
+export const COMPUTERNAME = "DESKTOP-CPAOK5R";
+
+/** @type {import('$env/static/private').ComSpec} */
+export const ComSpec = "C:\\Windows\\system32\\cmd.exe";
+
+/** @type {import('$env/static/private').DriverData} */
+export const DriverData = "C:\\Windows\\System32\\Drivers\\DriverData";
+
+/** @type {import('$env/static/private').EDITOR} */
+export const EDITOR = "notepad.exe";
+
+/** @type {import('$env/static/private').FPS_BROWSER_APP_PROFILE_STRING} */
+export const FPS_BROWSER_APP_PROFILE_STRING = "Internet Explorer";
+
+/** @type {import('$env/static/private').FPS_BROWSER_USER_PROFILE_STRING} */
+export const FPS_BROWSER_USER_PROFILE_STRING = "Default";
+
+/** @type {import('$env/static/private').GIT_ASKPASS} */
+export const GIT_ASKPASS = "c:\\Users\\박상훈 Pro\\AppData\\Local\\Programs\\Microsoft VS Code\\resources\\app\\extensions\\git\\dist\\askpass.sh";
+
+/** @type {import('$env/static/private').HOME} */
+export const HOME = "C:\\Users\\박상훈 Pro";
+
+/** @type {import('$env/static/private').HOMEDRIVE} */
+export const HOMEDRIVE = "C:";
+
+/** @type {import('$env/static/private').HOMEPATH} */
+export const HOMEPATH = "\\Users\\박상훈 Pro";
+
+/** @type {import('$env/static/private').INIT_CWD} */
+export const INIT_CWD = "C:\\Users\\박상훈 Pro\\vscodepj\\ddanggyeoket-client-2";
+
+/** @type {import('$env/static/private').JAVA_HOME} */
+export const JAVA_HOME = "C:\\setUpFile\\openjdk-11.0.1_windows-x64_bin\\jdk-11.0.1";
+
+/** @type {import('$env/static/private').LANG} */
+export const LANG = "ko_KR.UTF-8";
+
+/** @type {import('$env/static/private').LOCALAPPDATA} */
+export const LOCALAPPDATA = "C:\\Users\\박상훈 Pro\\AppData\\Local";
+
+/** @type {import('$env/static/private').LOGONSERVER} */
+export const LOGONSERVER = "\\\\DESKTOP-CPAOK5R";
+
+/** @type {import('$env/static/private').NODE} */
+export const NODE = "C:\\Program Files\\nodejs\\node.exe";
+
+/** @type {import('$env/static/private').NODE_EXE} */
+export const NODE_EXE = "C:\\Program Files\\nodejs\\\\node.exe";
+
+/** @type {import('$env/static/private').NPM_CLI_JS} */
+export const NPM_CLI_JS = "C:\\Program Files\\nodejs\\\\node_modules\\npm\\bin\\npm-cli.js";
+
+/** @type {import('$env/static/private').npm_command} */
+export const npm_command = "install";
+
+/** @type {import('$env/static/private').npm_config_cache} */
+export const npm_config_cache = "C:\\Users\\박상훈 Pro\\AppData\\Local\\npm-cache";
+
+/** @type {import('$env/static/private').npm_config_engine_strict} */
+export const npm_config_engine_strict = "true";
+
+/** @type {import('$env/static/private').npm_config_globalconfig} */
+export const npm_config_globalconfig = "C:\\Users\\박상훈 Pro\\AppData\\Roaming\\npm\\etc\\npmrc";
+
+/** @type {import('$env/static/private').npm_config_global_prefix} */
+export const npm_config_global_prefix = "C:\\Users\\박상훈 Pro\\AppData\\Roaming\\npm";
+
+/** @type {import('$env/static/private').npm_config_init_module} */
+export const npm_config_init_module = "C:\\Users\\박상훈 Pro\\.npm-init.js";
+
+/** @type {import('$env/static/private').npm_config_local_prefix} */
+export const npm_config_local_prefix = "C:\\Users\\박상훈 Pro\\vscodepj\\ddanggyeoket-client-2";
+
+/** @type {import('$env/static/private').npm_config_metrics_registry} */
+export const npm_config_metrics_registry = "https://registry.npmjs.org/";
+
+/** @type {import('$env/static/private').npm_config_node_gyp} */
+export const npm_config_node_gyp = "C:\\Program Files\\nodejs\\node_modules\\npm\\node_modules\\node-gyp\\bin\\node-gyp.js";
+
+/** @type {import('$env/static/private').npm_config_noproxy} */
+export const npm_config_noproxy = "";
+
+/** @type {import('$env/static/private').npm_config_prefix} */
+export const npm_config_prefix = "C:\\Users\\박상훈 Pro\\AppData\\Roaming\\npm";
+
+/** @type {import('$env/static/private').npm_config_userconfig} */
+export const npm_config_userconfig = "C:\\Users\\박상훈 Pro\\.npmrc";
+
+/** @type {import('$env/static/private').npm_config_user_agent} */
+export const npm_config_user_agent = "npm/8.11.0 node/v16.15.1 win32 x64 workspaces/false";
+
+/** @type {import('$env/static/private').npm_execpath} */
+export const npm_execpath = "C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js";
+
+/** @type {import('$env/static/private').npm_lifecycle_event} */
+export const npm_lifecycle_event = "postinstall";
+
+/** @type {import('$env/static/private').npm_lifecycle_script} */
+export const npm_lifecycle_script = "node svelte-kit.js sync";
+
+/** @type {import('$env/static/private').npm_node_execpath} */
+export const npm_node_execpath = "C:\\Program Files\\nodejs\\node.exe";
+
+/** @type {import('$env/static/private').npm_package_dev} */
+export const npm_package_dev = "1";
+
+/** @type {import('$env/static/private').npm_package_dev_optional} */
+export const npm_package_dev_optional = "";
+
+/** @type {import('$env/static/private').npm_package_engines_node} */
+export const npm_package_engines_node = ">=16.9";
+
+/** @type {import('$env/static/private').npm_package_integrity} */
+export const npm_package_integrity = "sha512-pKlmthl1SZkbx671Jp+LBoRne0vNzsjSgta9iRhqW/bt/0mx/IjlMd/NOeLuJGo30dAJdefrySoSamiaq47M/g==";
+
+/** @type {import('$env/static/private').npm_package_json} */
+export const npm_package_json = "C:\\Users\\박상훈 Pro\\vscodepj\\ddanggyeoket-client-2\\node_modules\\@sveltejs\\kit\\package.json";
+
+/** @type {import('$env/static/private').npm_package_name} */
+export const npm_package_name = "@sveltejs/kit";
+
+/** @type {import('$env/static/private').npm_package_optional} */
+export const npm_package_optional = "";
+
+/** @type {import('$env/static/private').npm_package_peer} */
+export const npm_package_peer = "";
+
+/** @type {import('$env/static/private').npm_package_resolved} */
+export const npm_package_resolved = "https://registry.npmjs.org/@sveltejs/kit/-/kit-1.0.0-next.403.tgz";
+
+/** @type {import('$env/static/private').npm_package_version} */
+export const npm_package_version = "1.0.0-next.403";
+
+/** @type {import('$env/static/private').NPM_PREFIX_NPM_CLI_JS} */
+export const NPM_PREFIX_NPM_CLI_JS = "C:\\Users\\諛뺤긽??Pro\\AppData\\Roaming\\npm\\node_modules\\npm\\bin\\npm-cli.js";
+
+/** @type {import('$env/static/private').NUMBER_OF_PROCESSORS} */
+export const NUMBER_OF_PROCESSORS = "16";
+
+/** @type {import('$env/static/private').OneDrive} */
+export const OneDrive = "C:\\Users\\박상훈 Pro\\OneDrive";
+
+/** @type {import('$env/static/private').ORIGINAL_XDG_CURRENT_DESKTOP} */
+export const ORIGINAL_XDG_CURRENT_DESKTOP = "undefined";
+
+/** @type {import('$env/static/private').OS} */
+export const OS = "Windows_NT";
+
+/** @type {import('$env/static/private').Path} */
+export const Path = "C:\\Users\\박상훈 Pro\\vscodepj\\ddanggyeoket-client-2\\node_modules\\@sveltejs\\kit\\node_modules\\.bin;C:\\Users\\박상훈 Pro\\vscodepj\\ddanggyeoket-client-2\\node_modules\\@sveltejs\\node_modules\\.bin;C:\\Users\\박상훈 Pro\\vscodepj\\ddanggyeoket-client-2\\node_modules\\node_modules\\.bin;C:\\Users\\박상훈 Pro\\vscodepj\\ddanggyeoket-client-2\\node_modules\\.bin;C:\\Users\\박상훈 Pro\\vscodepj\\node_modules\\.bin;C:\\Users\\박상훈 Pro\\node_modules\\.bin;C:\\Users\\node_modules\\.bin;C:\\node_modules\\.bin;C:\\Program Files\\nodejs\\node_modules\\npm\\node_modules\\@npmcli\\run-script\\lib\\node-gyp-bin;C:\\Windows\\system32;C:\\Windows;C:\\Windows\\System32\\Wbem;C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\;C:\\Windows\\System32\\OpenSSH\\;C:\\Program Files (x86)\\NVIDIA Corporation\\PhysX\\Common;C:\\Program Files\\NVIDIA Corporation\\NVIDIA NvDLISR;C:\\Program Files\\dotnet\\;C:\\setUpFile\\openjdk-11.0.1_windows-x64_bin\\jdk-11.0.1\\bin;C:\\Program Files\\nodejs\\;C:\\Program Files\\Git\\cmd;C:\\Program Files\\Docker\\Docker\\resources\\bin;C:\\ProgramData\\DockerDesktop\\version-bin;C:\\Users\\박상훈 Pro\\AppData\\Local\\Microsoft\\WindowsApps;C:\\Users\\박상훈 Pro\\AppData\\Roaming\\npm;C:\\Users\\박상훈 Pro\\AppData\\Local\\Programs\\Microsoft VS Code\\bin";
+
+/** @type {import('$env/static/private').PATHEXT} */
+export const PATHEXT = ".COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC;.CPL";
+
+/** @type {import('$env/static/private').PROCESSOR_ARCHITECTURE} */
+export const PROCESSOR_ARCHITECTURE = "AMD64";
+
+/** @type {import('$env/static/private').PROCESSOR_IDENTIFIER} */
+export const PROCESSOR_IDENTIFIER = "Intel64 Family 6 Model 141 Stepping 1, GenuineIntel";
+
+/** @type {import('$env/static/private').PROCESSOR_LEVEL} */
+export const PROCESSOR_LEVEL = "6";
+
+/** @type {import('$env/static/private').PROCESSOR_REVISION} */
+export const PROCESSOR_REVISION = "8d01";
+
+/** @type {import('$env/static/private').ProgramData} */
+export const ProgramData = "C:\\ProgramData";
+
+/** @type {import('$env/static/private').ProgramFiles} */
+export const ProgramFiles = "C:\\Program Files";
+
+/** @type {import('$env/static/private').ProgramW6432} */
+export const ProgramW6432 = "C:\\Program Files";
+
+/** @type {import('$env/static/private').PROMPT} */
+export const PROMPT = "$P$G";
+
+/** @type {import('$env/static/private').PSModulePath} */
+export const PSModulePath = "C:\\Users\\박상훈 Pro\\Documents\\WindowsPowerShell\\Modules;C:\\Program Files\\WindowsPowerShell\\Modules;C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\Modules";
+
+/** @type {import('$env/static/private').PUBLIC} */
+export const PUBLIC = "C:\\Users\\Public";
+
+/** @type {import('$env/static/private').SESSIONNAME} */
+export const SESSIONNAME = "Console";
+
+/** @type {import('$env/static/private').SystemDrive} */
+export const SystemDrive = "C:";
+
+/** @type {import('$env/static/private').SystemRoot} */
+export const SystemRoot = "C:\\Windows";
+
+/** @type {import('$env/static/private').TEMP} */
+export const TEMP = "C:\\Users\\박상훈~1\\AppData\\Local\\Temp";
+
+/** @type {import('$env/static/private').TERM_PROGRAM} */
+export const TERM_PROGRAM = "vscode";
+
+/** @type {import('$env/static/private').TERM_PROGRAM_VERSION} */
+export const TERM_PROGRAM_VERSION = "1.69.2";
+
+/** @type {import('$env/static/private').TMP} */
+export const TMP = "C:\\Users\\박상훈~1\\AppData\\Local\\Temp";
+
+/** @type {import('$env/static/private').USERDOMAIN} */
+export const USERDOMAIN = "DESKTOP-CPAOK5R";
+
+/** @type {import('$env/static/private').USERDOMAIN_ROAMINGPROFILE} */
+export const USERDOMAIN_ROAMINGPROFILE = "DESKTOP-CPAOK5R";
+
+/** @type {import('$env/static/private').USERNAME} */
+export const USERNAME = "박상훈 Pro";
+
+/** @type {import('$env/static/private').USERPROFILE} */
+export const USERPROFILE = "C:\\Users\\박상훈 Pro";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_EXTRA_ARGS} */
+export const VSCODE_GIT_ASKPASS_EXTRA_ARGS = "--ms-enable-electron-run-as-node";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_MAIN} */
+export const VSCODE_GIT_ASKPASS_MAIN = "c:\\Users\\박상훈 Pro\\AppData\\Local\\Programs\\Microsoft VS Code\\resources\\app\\extensions\\git\\dist\\askpass-main.js";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_NODE} */
+export const VSCODE_GIT_ASKPASS_NODE = "C:\\Users\\박상훈 Pro\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe";
+
+/** @type {import('$env/static/private').VSCODE_GIT_IPC_HANDLE} */
+export const VSCODE_GIT_IPC_HANDLE = "\\\\.\\pipe\\vscode-git-eff33443cb-sock";
+
+/** @type {import('$env/static/private').windir} */
+export const windir = "C:\\Windows";
+
+/** @type {import('$env/static/private').ZES_ENABLE_SYSMAN} */
+export const ZES_ENABLE_SYSMAN = "1";
